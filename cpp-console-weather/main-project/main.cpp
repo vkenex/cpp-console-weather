adab78a7f926083780197c6adc19283f5ee3175c
vkenex <vkenex010@gmail.com>
2023-12-18 20:26:18 +0300

    Добавлены тестовые данные

--- cpp-console-weather/main-project/main.cpp
+++ cpp-console-weather/main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #3. Weather\n";
     cout << "Author: Klimov Vladislav\n";
+    cout << "Group: 23PInj1D_1\n";
     return 0;
 }
