a890075e9b48ce7251d656cd54e08c24e3140dd4
vkenex <153535883+vkenex@users.noreply.github.com>
2023-12-18 19:10:48 +0300

    Добавлен вывод общей информации о задании

--- cpp-console-weather/main-project/main.cpp
+++ cpp-console-weather/main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #3. Weather\n";
+    cout << "Author: Klimov Vladislav\n";
     return 0;
 }
