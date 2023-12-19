dc0ea67d021575ab4ad629f3dba8a433046d619f
vkenex <vkenex010@gmail.com>
2023-12-19 18:06:52 +0300

    Создан прототип теста

---
+++ cpp-console-weather/unit-test-project/unit-test-project.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittestproject
+{
+	TEST_CLASS(unittestproject)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::Fail();
+		}
+	};
+}
